9be22c97e2c71ebc0efdba06f9d9bce08f353282
Olga <olgamaslakowa2002@yandex.ru>
2025-06-02 22:25:42 +0300

    add files

--- src/s21_math.h
+++ src/s21_math.h
@@ -13,24 +13,24 @@
 #define EPS_6 1e-06
 
 bool is_inf(long double x);
-int s21_abs(int x);              // sbermax   //ramonagr
-long double s21_acos(double x);  // sbermax   //ramonagr
-long double s21_asin(double x);  // sbermax   //ramonagr
-long double s21_atan(double x);  // sbermax   //ramonagr
+int s21_abs(int x);              
+long double s21_acos(double x);  
+long double s21_asin(double x);  
+long double s21_atan(double x);  
 
-long double s21_ceil(double x);   // olya_afel //ramonagr
-long double s21_cos(double x);    // olya_afel //ramonagr
-long double s21_exp(double x);    // olya_afel //ramonagr
-long double s21_fabs(double x);   // olya_afel //ramonagr
-long double s21_floor(double x);  // olya_afel //Kanatik14
+long double s21_ceil(double x);   
+long double s21_cos(double x);    
+long double s21_exp(double x);    
+long double s21_fabs(double x);   
+long double s21_floor(double x);  
 long int s21_factorial(int number);
 
-long double s21_fmod(double x, double y);      // sappzhn   //Kanatik14
-long double s21_log(double x);                 // sappzhn   //Kanatik14
-long double s21_pow(double base, double exp);  // sappzhn   //Kanatik14
-long double s21_sin(double x);                 // sappzhn   //Kanatik14
-long double s21_sqrt(double x);                // sappzhn   //Kanatik14
-long double s21_tan(double x);                 // sappzhn   //Kanatik14
+long double s21_fmod(double x, double y);      
+long double s21_log(double x);                 
+long double s21_pow(double base, double exp);  
+long double s21_sin(double x);                 
+long double s21_sqrt(double x);                
+long double s21_tan(double x);                 
 
 int sign(double x);
 int is_odd_int(double y);
